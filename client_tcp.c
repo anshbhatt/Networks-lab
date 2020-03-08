39990a67af95f9e62ec4c7591aa470bd98253a20
Jayant Patidar <jpatidar8740@gmail.com>
2020-03-08 23:38:10 +0530

    Rough code

--- client_tcp.c
+++ client_tcp.c
@@ -1,36 +1,71 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<stdio.h>
 
-#include <sys/types.h>
-#include <sys/socket.h>
+#define MAX_LENGTH 500
 
-#include <netinet/in.h>
 
-int main()
-{ 
-	// Socket Setup Stage
-	int client_socket;
-	client_socket = socket(AF_INET , SOCK_STREAM , 0);
+void err_handler(char err[255]) {
+	printf("Error: %s\n", err);
+	exit(1);
+}
 
-	struct sockaddr_in server_address;
-	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(9002);
-	server_address.sin_addr.s_addr = INADDR_ANY;
+int create_udp_socket(int port) {
+	int cli_socket;
+	cli_socket = socket(AF_INET , SOCK_DGRAM, 0);
 
-	int connection_status = connect(client_socket , (struct sockaddr *) &server_address , sizeof(server_address));
+	struct sockaddr_in cli_address;
+	cli_address.sin_family = AF_INET;
+	cli_address.sin_port = htons(port);
+	cli_address.sin_addr.s_addr = INADDR_ANY;
 
-	if (connection_status == -1){
-		printf("Error connecting to the server \n\n");
-	}
-	else{
-		char response[256];
-		recv(client_socket , &response , sizeof(response) , 0);
+	int status = bind(cli_socket , (struct sockaddr *) &cli_address , sizeof(cli_address));
+	if(status == -1) err_handler("Binding failed");
+	return cli_socket;
+}
 
-		printf("Data recieved : %s\n", response);
-	}
 
-	close(client_socket);
+int main() {
+    int sock_id = socket(PF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in addrport;
+    addrport.sin_family = AF_INET;
+    addrport.sin_port = htons(3002);
+    addrport.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	return 0;
+    int status = connect(sock_id, (struct sockaddr *) &addrport, sizeof(addrport));
+
+    if(status == -1) {
+        printf("Error connecting\n");
+        return 0;
+    }
+
+    char buff[1024];
+
+    char echo_word[25];
+    int rcvMsgSize;
+        fgets(echo_word, 25, stdin);
+        printf("%s  Sent\n", echo_word);
+        if(send(sock_id, echo_word, 25, 0) != 25){ printf("Error sending msg\n"); return 0;}
+        if((rcvMsgSize = recv(sock_id, buff, 1024, 0)) < 0) {
+            printf("Error receiving msg\n"); return 0;
+        };
+        printf("%s Rcvd\n", buff);
+        close(sock_id);
+
+        // TODO: ceate fun which decode string then create udp port send msg
+
+        int port = atoi(buff);
+        printf("%d\n", port);
+        addrport.sin_port = htons(port);
+        int p;
+        scanf("%d", &p);
+        int udp_fd = create_udp_socket(p);
+        char msg[MAX_LENGTH];
+        sendto(udp_fd, "4000", 4, MSG_CONFIRM, (struct sockaddr *) &addrport, sizeof(addrport));
+        printf("error\n");
+        int count = recvfrom(udp_fd, (char *)buff, MAX_LENGTH, MSG_WAITALL, (struct sockaddr *) &addrport, sizeof(addrport));
+        printf("%s jkj\n", buff);
+        printf("%d\n", count);
+        close(udp_fd);
+
 }
--- server_tcp.c
+++ server_tcp.c
@@ -1,32 +1,113 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 #include <unistd.h>
+#include<string.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
 
 #include <netinet/in.h>
 
-int main()
-{ 
-	char message[256] = "Response from server : Hello World ";
+#define MAX_PENDING 128
+#define MAX_LENGTH 1024
 
+char buffer[MAX_LENGTH];
+
+void err_handler(char err[255]) {
+	printf("Error: %s\n", err);
+	exit(1);
+}
+
+struct Msg {
+	int msg_type;
+	int msg_length;
+	char msg[MAX_LENGTH];
+};
+
+int create_tcp_socket(int port) {
 	int server_socket;
 	server_socket = socket(AF_INET , SOCK_STREAM , 0);
 
 	struct sockaddr_in server_address;
 	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(9002);
+	server_address.sin_port = htons(port);
 	server_address.sin_addr.s_addr = INADDR_ANY;
 
-	bind(server_socket , (struct sockaddr *) &server_address , sizeof(server_address));
-	listen(server_socket , 5);
+	int status = bind(server_socket , (struct sockaddr *) &server_address , sizeof(server_address));
+	if(status == -1) err_handler("Binding failed");
+	return server_socket;
+}
 
-	int client_socket;
-	client_socket = accept(server_socket , NULL , NULL);
 
-	send(client_socket , message , sizeof(message) , 0);
+int create_udp_socket(int port) {
+	int server_socket;
+	server_socket = socket(AF_INET , SOCK_DGRAM, 0);
 
-	close(server_socket);
+	struct sockaddr_in server_address;
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(port);
+	server_address.sin_addr.s_addr = INADDR_ANY;
+
+	int status = bind(server_socket , (struct sockaddr *) &server_address , sizeof(server_address));
+	if(status == -1) err_handler("Binding failed");
+	return server_socket;
+}
+
+
+
+int main()
+{
+	int tcp_fd = create_tcp_socket(3002);
+	int udp_fd = create_udp_socket(3003);
+
+	while(listen(tcp_fd , MAX_PENDING) != -1) {
+		struct sockaddr_in cli_addr;
+		int len = sizeof(cli_addr);
+		int cli_id = accept(tcp_fd, (struct sockaddr *) &cli_addr, &len);
+		inet_ntop(AF_INET, &cli_addr.sin_addr, buffer, INET_ADDRSTRLEN);
+
+		pid_t pid = fork();
+		printf("%d\n", pid);
+		if(pid == 0) {
+
+			int rcvMsgSize;
+			printf("connection from: %s, port: %d\n", buffer, ntohs(cli_addr.sin_port));
+
+			if((rcvMsgSize = recv(cli_id, buffer, MAX_LENGTH, 0)) < 0) err_handler("Error in recv()");
+
+			char repl[MAX_LENGTH];
+			//TODO: create a function whick encode msg in string
+			/*strcat(repl, "Type:1*");
+			strcat(repl, "Msg Length:4*");
+			strcat(repl, "Msg:3003");
+			printf("%d\n", strlen(repl));*/
+
+			if(send(cli_id, "3003", 4, 0) != 4) err_handler("Error in sending bytes");
+
+
+			//Receive from client
+			int udp_msg_length = 0, cli_udp_id;
+			int flags = 0;
+			int count = recvfrom(udp_fd, (char *)buffer, MAX_LENGTH, 0, (struct sockaddr *) &cli_addr, &len);
+			printf("%d\n", count);
+			int port = atoi(buffer);
+
+			printf("%d\n", port);
+
+			struct sockaddr_in addrport;
+			addrport.sin_family = AF_INET;
+			addrport.sin_port = htons(port);
+			addrport.sin_addr.s_addr = htonl(INADDR_ANY);
+
+			sendto(udp_fd, "Hello", 5, 0, (struct sockaddr *) &addrport, sizeof(addrport));
+			printf("Hello message sent.\n");
+
+			exit(EXIT_SUCCESS);
+		}
+
+	}
+
+
 	return 0;
 }
