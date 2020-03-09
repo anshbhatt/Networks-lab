2e41af05e8fd2e08094ba3012a5900c95530184d
Jayant Patidar <jpatidar8740@gmail.com>
2020-03-09 11:54:43 +0530

    Decode and Encode functions added

--- server_tcp.c
+++ server_tcp.c
@@ -19,11 +19,75 @@ void err_handler(char err[255]) {
 	exit(1);
 }
 
-struct Msg {
-	int msg_type;
-	int msg_length;
-	char msg[MAX_LENGTH];
-};
+// C substring function definition
+void substring(char s[], char sub[], int p, int l) {
+   int c = 0;
+
+   while (p+c <= l) {
+      sub[c] = s[p+c];
+      c++;
+			if(s[p+c] == '\0') break;
+   }
+   sub[c] = '\0';
+}
+
+// return pos of delimiters (if which_del is 1 => return first del's pos and so on)
+// here : is also del since in (Type:1) here info after : is useful
+int pos_of_del(char *str, int which_del, char del) {
+	int i=0;
+	while(str[i] != '\0') {
+		if(which_del == 0) return i-1;
+		if(str[i] == del) which_del--;
+		i++;
+	}
+}
+
+char *itoa(int num, char *str)
+{
+        if(str == NULL)
+        {
+                return NULL;
+        }
+        sprintf(str, "%d", num);
+        return str;
+}
+
+void encode_msg(int t, int len, char * msg, char *res) {
+	// ~ is delimiter
+	char type[4]="";
+
+	strcat(res, "Type:"); strcat(res, (char *)itoa(t, type));
+	strcat(res, "~");
+	char length[4]="";
+	strcat(res, "Length:"); strcat(res, (char *)itoa(len, length));
+	strcat(res, "~");
+	strcat(res, "Msg:"); strcat(res, msg);
+	return;
+}
+
+void decode_msg(char *str, int *type, int *length, char *msg) {
+	int i=0;
+	// first : and first ~ in between is type
+	int first_del = pos_of_del(str, 1, ':'), sec_del = pos_of_del(str, 1, '~');
+	char type_str[128]="";
+	substring(str, type_str, first_del+1, sec_del-1);
+	*type = atoi(type_str);
+
+
+
+	char length_str[128]="";
+	first_del = pos_of_del(str, 2, ':'); sec_del=pos_of_del(str, 2, '~');
+	substring(str, length_str, first_del+1, sec_del-1);
+	*length = atoi(length_str);
+
+
+	int msg_start = pos_of_del(str, 3, ':');
+	char msg_str[1024]="";
+	substring(str, msg_str, msg_start+1, 1025);
+	strcat(msg, msg_str);
+
+	return;
+}
 
 int create_tcp_socket(int port) {
 	int server_socket;
@@ -61,6 +125,15 @@ int main()
 	int tcp_fd = create_tcp_socket(3002);
 	int udp_fd = create_udp_socket(3003);
 
+	char msg[1024]="", res[1024]="";
+	encode_msg(1, 5, "Hello Dear", res);
+	printf("%s\n", res);
+
+	int t, l;
+	decode_msg(res, &t, &l, msg);
+	printf("%d  %d  %s\n", t, l, msg);
+
+	/*
 	while(listen(tcp_fd , MAX_PENDING) != -1) {
 		struct sockaddr_in cli_addr;
 		int len = sizeof(cli_addr);
@@ -81,7 +154,7 @@ int main()
 			/*strcat(repl, "Type:1*");
 			strcat(repl, "Msg Length:4*");
 			strcat(repl, "Msg:3003");
-			printf("%d\n", strlen(repl));*/
+			printf("%d\n", strlen(repl));
 
 			if(send(cli_id, "3003", 4, 0) != 4) err_handler("Error in sending bytes");
 
@@ -104,9 +177,9 @@ int main()
 			printf("Hello message sent.\n");
 
 			exit(EXIT_SUCCESS);
-		}
+		}*/
 
-	}
+
 
 
 	return 0;
